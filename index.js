8dc74c4cbc1565fffb8425062a631e1be62b9f38
BahAilime <emilia.beguin@gmail.com>
2024-09-28 16:05:34 +0200

    Fix fetchHTML params

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ import { parseHTML } from './htmlParser.js';
 import { fetchHTML } from './api.js';
 
 async function main() {
-    const html = await fetchHTML(week, credentials);
+    // const html = await fetchHTML(week, credentials);
+    const html = await fetchHTML();
     console.log(await parseHTML(html));
 }
 
 main();
-
