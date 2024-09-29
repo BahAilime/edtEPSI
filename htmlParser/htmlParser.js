386e9dbdcc77b8e921be2b2735193e7f5e7fbd78
BahAilime <emilia.beguin@gmail.com>
2024-09-29 13:26:03 +0200

    better template for schedule-x (see coursEPSI)

--- htmlParser/htmlParser.js
+++ htmlParser/htmlParser.js
@@ -1,6 +1,7 @@
 import { JSDOM } from 'jsdom';
+import { hashCode } from '../utils/utils.js';
 
-function convertDate(dateString) {
+function convertDate(dateString, time) {
     const months = {
         "Janvier": "01",
         "Février": "02",
@@ -16,13 +17,22 @@ function convertDate(dateString) {
         "Décembre": "12"
     };
 
+    const startTimeHours = time[0].split(":")[0];
+    const startTimeMinutes = time[0].split(":")[1];
+    const endTimeHours = time[1].split(":")[0];
+    const endTimeMinutes = time[1].split(":")[1];
+
     let dateParts = dateString.split(" ");
     let day = dateParts[1];         // Get the day
     let month = dateParts[2];       // Get the month
 
     let monthNumber = months[month];
 
-    return `${day}/${monthNumber}`;
+
+    return [
+        new Date(2024, monthNumber-1, day, startTimeHours, startTimeMinutes),
+        new Date(2024, monthNumber-1, day, endTimeHours, endTimeMinutes)
+    ];
 }
 
 function extractLocation(locationString) {
@@ -57,13 +67,14 @@ export async function parseHTML(html) {
     try {
         const dataPageDocument = new JSDOM(html).window.document;
 
-        if (html.search("Pas de cours cette semaine")) {
-            return {
-                error: "Pas de cours cette semaine"
-            }
-        }
+        // if (html.search("Pas de cours cette semaine")) {
+        //     return {
+        //         error: "Pas de cours cette semaine"
+        //     }
+        // }
 
-        const caseElements = dataPageDocument.querySelectorAll('.Case');
+        let caseElements = dataPageDocument.querySelectorAll('.Case');
+        caseElements = Array.from(caseElements).slice(0, -1);
         const dateElements = dataPageDocument.querySelectorAll('.TCJour');
         const dates = Array.from(dateElements).map(element => element.textContent).slice(5, 10);
         const leftToDayMap = {
@@ -84,46 +95,56 @@ export async function parseHTML(html) {
             }
 
             // Extract prof and group
+            let professor = "";
             const groupHTML = event.querySelector('td.TCProf')
-            let group = ["", ""];
+            let group = "";
             if (groupHTML) {
                 let wordsToRemove = ["24/25", "EPSI", "NTE", "DEVIA", "BACHELOR", "3", "ALT"];
                 let span = groupHTML.querySelector('span');
                 if (span) span.remove();
                 let text = groupHTML.innerHTML;
-                group = text.split('<br>').map(text => text.trim());
+                let profAndGroup = text.split('<br>').map(text => text.trim());
 
-                group[0] = capitalizeName(group[0].split(' ').reverse().join(' '));
+                professor = capitalizeName(profAndGroup[0].split(' ').reverse().join(' '));
 
-                group[1] = group[1].split(' ')
+                group = profAndGroup[1].split(' ')
                     .filter(word => !wordsToRemove.includes(word))
                     .join(' ');
             }
 
-            let time = event.querySelector('td.TChdeb')?.textContent.trim().split(" - ");  // EX "09:00 - 12:00"
-            if (time == undefined) {
-                time = ["", ""]
-            }
-
             let location = extractLocation(event.querySelector('td.TCSalle')?.textContent.trim());  // EX "Salle:N211(HEP Nantes)"         
+
+            let time = event.querySelector('td.TChdeb')?.textContent.trim().split(" - ");
+            if (time == undefined) {
+                time = ["00:00", "00:01"]
+            }
 
             const styleAttr = event.attributes['style'];
             const match = styleAttr && styleAttr.value.match(/left:(\d+)/);
             const leftValue = match && match[1].substring(0, 3);
             let day = leftToDayMap[leftValue];
             if (day) {
-                day = convertDate(day);
+                day = convertDate(day, time);
+                
             }
-
+            
+                
             return {
-                title: title || '',
-                prof: group?.[0] || '',
-                group: group?.[1] || '',
-                day: day || '',
-                start: time[0] || '',
-                end: time[1] || '',
-                location: location || ''
-            };
+                id: hashCode(title),         
+                title: title,
+                start: day[0],         // Start date and time (JavaScript Date object)
+                end: day[1],           // End date and time (JavaScript Date object)
+                location: location,
+                organizer: {
+                    name: professor,
+                },
+                attendees: [
+                    {
+                        name: group,
+                    },
+                ],
+                color: '#ffffff',          // Event color for display
+            }
 
         }).filter(event => event.title !== "");
 
